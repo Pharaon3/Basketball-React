7f445b58fce4f7b2fe006413ee1be088e59cfefb
pharaon3 <pharaon313@outlook.com>
2023-03-05 10:17:06 +0900

    /main issue fixed

--- src/Routing.js
+++ src/Routing.js
@@ -55,20 +55,21 @@ function Routing({fullScreenHandle}) {
             path="/main"
             element={
               <MainPage
-                fieldLineFlag={fieldLineFlag}  setFieldLineFlag={setFieldLineFlag}
-                fullScreenFlag={fullScreenFlag} setFullScreenFlag={setFullScreenFlag}
-                fullScreenHandle={fullScreenHandle}
+              fieldLineFlag={fieldLineFlag}  setFieldLineFlag={setFieldLineFlag}
+              fullScreenFlag={fullScreenFlag} setFullScreenFlag={setFullScreenFlag}
+              fullScreenHandle={fullScreenHandle}
 
-                imgWidth={imgWidth} setImgWidth={setImgWidth}
-                windowsWidth={windowsWidth} setWindowsWidth={setWindowsWidth}
-                mousePosX={mousePosX} setMousePosX={setMousePosX}
-                mousePosY={mousePosY} setMousePosY={setMousePosY}
-                newCircles={newCircles} setNewCircles={setNewCircles}
-                newPoints={newPoints} setNewPoints={setNewPoints}
-                newBalls={newBalls} setNewBalls={setNewBalls}
-                positionCircleDiff={positionCircleDiff} setPositionCircleDiff={setPositionCircleDiff}
-                positionPointDiff={positionPointDiff} setPositionPointDiff={setPositionPointDiff}
-                positionBallDiff={positionBallDiff} setPositionBallDiff={setPositionBallDiff}
+              imgWidth={imgWidth} setImgWidth={setImgWidth}
+              imgHeight={imgHeight} setImgHeight={setImgHeight}
+              windowsWidth={windowsWidth} setWindowsWidth={setWindowsWidth}
+              mousePosX={mousePosX} setMousePosX={setMousePosX}
+              mousePosY={mousePosY} setMousePosY={setMousePosY}
+              newCircles={newCircles} setNewCircles={setNewCircles}
+              newPoints={newPoints} setNewPoints={setNewPoints}
+              newBalls={newBalls} setNewBalls={setNewBalls}
+              positionCircleDiff={positionCircleDiff} setPositionCircleDiff={setPositionCircleDiff}
+              positionPointDiff={positionPointDiff} setPositionPointDiff={setPositionPointDiff}
+              positionBallDiff={positionBallDiff} setPositionBallDiff={setPositionBallDiff}
               />
             }
           />}
