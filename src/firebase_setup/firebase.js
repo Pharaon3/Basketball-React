304ac17752c5e7e3124ee59b227531e4940f6c9a
albertobrooks <alb.brooks@outlook.com>
2023-03-21 11:54:18 +0900

    code clean in firebase

--- src/firebase_setup/firebase.js
+++ src/firebase_setup/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore, collection, getDoc, getDocs } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,4 +20,3 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const firestore = getFirestore(app);
-const analytics = getAnalytics(app);
